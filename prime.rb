c01cc91f8e60cdec7819441a3ddbf323f1fc3c4a
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-10 12:35:42 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,1 +1,18 @@
-# Add  code here!
+require 'pry'
+def prime?(integer) 
+  if integer == 0||1||2
+    return true
+  end
+  
+ nums=(2..integer)
+  nums.each do |number|
+  # number cant be div by another #
+   if integer % number== 0 
+    return false 
+   else 
+     true
+   end
+    
+  end
+   true
+end
