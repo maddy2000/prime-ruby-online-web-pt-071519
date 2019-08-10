0f55666dcfd03f3ece167af5a5255b86969bd822
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-10 13:54:23 +0000

    Done.

--- prime.rb
+++ prime.rb
@@ -1,1 +1,19 @@
-# Add  code here!
+require 'pry'
+def prime?(integer) 
+  if integer < 0 || integer == 0 || integer == 1
+    return false
+end
+  
+ nums=(2..integer-1).to_a
+ #binding.pry
+  nums.each do |number|
+  # number cant be div evenly #
+   if integer % number== 0 
+    return false
+   else 
+     true
+   end
+    
+  end
+   true
+end
