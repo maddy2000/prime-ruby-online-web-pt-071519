10d9c84004a176e2b0a15b4117b2ccfb76e51d7b
Madelyn Deshield <madelynwallop@gmail.com>
2019-08-10 13:31:18 +0000

    Automatically backed up by Learn

--- prime.rb
+++ prime.rb
@@ -1,14 +1,14 @@
 require 'pry'
 def prime?(integer) 
   if integer == -2
-    return true
+    return false
 end
   
  nums=(2..integer)
   nums.each do |number|
   # number cant be div evenly #
    if integer % number== 0 
-    return false 
+    return true
    else 
      true
    end
